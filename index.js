dbed30a90b4715b9f2f885d97d4cbc77f2910c9a
Oghovese Imetowa <itzoghuski@gmail.com>
2022-09-22 13:49:42 -0700

    made a play round function

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-let items = ['rock', 'paper' , 'scissor'];
-function getComputerChoice (items) {
-    return items[Math.floor(Math.random()*items.length)];
+const item = ['rock', 'paper' , 'scissor'];
+function getComputerChoice (item) {
+    return item[Math.floor(Math.random()*item.length)];
 
 }
+console.log(getComputerChoice(item))
+const playerSelection = prompt ("Rock paper Scissors ? "  );
+const computerSelection = getComputerChoice(item);
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == item[0] && computerSelection == item[0] ){
+        let display = "you guessed it  right"
+        return console.log (display)
+    }
+    else {
+        console.log ("you lose !!!")
+    }
+}
+
+console.log (playRound(playerSelection, computerSelection))
