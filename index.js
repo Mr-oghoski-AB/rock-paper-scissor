c4a905c99fdaf1d4fee45134c5280fadc0d4dfb6
Oghovese Imetowa <itzoghuski@gmail.com>
2022-09-22 14:12:48 -0700

    modified the play round function

--- index.js
+++ index.js
@@ -8,9 +8,28 @@ const playerSelection = prompt ("Rock paper Scissors ? "  );
 const computerSelection = getComputerChoice(item);
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == item[0] && computerSelection == item[0] ){
-        let display = "you guessed it  right"
-        return console.log (display)
+    if (playerSelection == computerSelection ){
+        let display = "its a tie "
+        return display
+    } 
+    else if (playerSelection == item[1] && computerSelection == item[0]) {
+        return "you WIN !!!!!! CONGRATULATIONS"
+
+    }
+    else if (playerSelection == item [0] && computerSelection == item [1]){
+        return "you lose!!!! paper beats rock "
+    }
+    else if (playerSelection == item [1] && computerSelection == item [2]){
+        return "you lose!!! scissors beats paper "
+    }
+    else if (playerSelection == item [2] && computerSelection == item[1]){
+        return "you WIN !!!!!CONGRATULATIONS "
+    }
+    else if (playerSelection == item [0] && computerSelection == item[2]) {
+        return "you WIN !!!!! CONGRATULATIONS "
+    }
+    else if ( playerSelection == item[2] && computerSelection == item [0]){
+        return "you LOSE !! Rock beats paper"
     }
     else {
         console.log ("you lose !!!")
