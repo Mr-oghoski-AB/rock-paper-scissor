4d830b87c893ee6c3e2c92a6265e521dad1e8252
Oghovese Imetowa <itzoghuski@gmail.com>
2022-09-26 02:46:15 -0700

    fixed the game loop of 5 rounds that keeps record of the score and report a winnner at the end

--- index.js
+++ index.js
@@ -4,38 +4,74 @@ function getComputerChoice (item) {
 
 }
 
-let playerSelection1 = prompt ("Rock Paper Scissor ? "  );
-const playerSelection = playerSelection1.toLowerCase()
+
+let playerSelection1 = prompt("Rock Paper Scissor ? ")  ;
+const playerSelection = playerSelection1.toLowerCase();
 const computerSelection = getComputerChoice(item);
+let computerScore = 0;  
+let playerScore = 0;
+let gameScores = `user: ${playerScore}  computer: ${computerScore}  ` 
+
 
 function playRound(playerSelection, computerSelection){
+
+
     if (playerSelection == computerSelection ){
         let display = "its a tie, Please try again "
         return display
     } 
     else if (playerSelection == item[1] && computerSelection == item[0]) {
-        return "you WIN !!!!!! CONGRATULATIONS"
+        playerScore += 1 
+        return "you WIN !!!!!! CONGRATULATIONS";          
 
     }
     else if (playerSelection == item [0] && computerSelection == item [1]){
-        return "you lose!!!! paper beats rock "
+        computerScore += 1
+        return "you lose!!!! paper beats rock ";
     }
     else if (playerSelection == item [1] && computerSelection == item [2]){
-        return "you lose!!! scissors beats paper "
+        computerScore += 1
+        return "you lose!!! scissors beats paper";
     }
     else if (playerSelection == item [2] && computerSelection == item[1]){
+        playerScore += 1
         return "you WIN !!!!!CONGRATULATIONS "
     }
     else if (playerSelection == item [0] && computerSelection == item[2]) {
-        return "you WIN !!!!! CONGRATULATIONS "
+        playerScore += 1
+        return "you WIN !!!!! CONGRATULATIONS ";
     }
     else if ( playerSelection == item[2] && computerSelection == item [0]){
-        return "you LOSE !! Rock beats paper"
+        computerScore += 1
+        return "you LOSE !! Rock beats paper";
     }
     else {
         return "Wrong input!!! please input Rock , Paper or Scissor"
     }
+
 }
-console.log (playRound(playerSelection, computerSelection))
-console.log("computer choice is " + getComputerChoice(item))
+
+
+console.log("computer choice is " + getComputerChoice(item));
+
+function game() {
+    for (let numberOfGames = 0; numberOfGames < 6; numberOfGames++ ) {
+        if (numberOfGames != 5) {
+    
+            console.log (playRound(playerSelection, computerSelection))
+            console.log (gameScores)
+      
+        }
+        else if (numberOfGames = 5 && playerScore > computerScore ) {
+            return "you Win!!"
+        }
+        else if (numberOfGames = 5 && computerScore > playerScore){
+            return "you lose!!!! Better luck next time"
+        }
+    }
+
+}
+console.log (game())
+
+
 
